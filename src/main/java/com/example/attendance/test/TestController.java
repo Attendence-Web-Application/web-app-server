8ee0632c5599d0adbb456e2155609d338e435191
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-03-07 12:27:38 -0800

    Example for springboot

--- src/main/java/com/example/attendance/test/TestController.java
+++ src/main/java/com/example/attendance/test/TestController.java
@@ -2,6 +2,7 @@ package com.example.attendance.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +18,31 @@ public class TestController {
         List all = testDao.findAll();
 
         return all;
+    }
+
+    @GetMapping("/add")
+    public Test add(@RequestParam("id") int id,
+                    @RequestParam("name") String name,
+                    @RequestParam("age") int age){
+        Test test = new Test();
+        test.setId(id);
+        test.setName(name);
+        test.setAge(age);
+
+        Test save = testDao.save(test);
+        return save;
+    }
+
+    @GetMapping("/deleteOne")
+    public String deleteOne(@RequestParam("id") int id){
+        testDao.deleteById(id);
+        return "delete success";
+    }
+
+    @GetMapping("/deleteAll")
+    public String deleteAll(){
+        testDao.deleteAll();
+        return "success delete all";
     }
 
 }
