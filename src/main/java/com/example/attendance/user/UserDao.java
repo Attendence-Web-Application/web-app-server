41f3d414016758d4657cb95bf6fd7332538a01a4
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-03-22 11:18:16 -0700

    Add: check whether or not username and email is already in use

--- src/main/java/com/example/attendance/user/UserDao.java
+++ src/main/java/com/example/attendance/user/UserDao.java
@@ -1,7 +1,12 @@
 package com.example.attendance.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDao extends JpaRepository<User, Integer> {
     User findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByName(String name);
 }
